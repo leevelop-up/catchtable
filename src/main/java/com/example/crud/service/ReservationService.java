893707c8991bc257b9af5a3503ea5324e7382743
unknown <merong0075@gmail.com>
2025-03-05 23:00:54 +0900

    redis 값으로 체크하도록 수정

--- src/main/java/com/example/crud/service/ReservationService.java
+++ src/main/java/com/example/crud/service/ReservationService.java
@@ -3,6 +3,7 @@ package com.example.crud.service;
 import com.example.crud.component.DistributeLockExecutor;
 import com.example.crud.domain.Reservation;
 import com.example.crud.domain.Shop;
+import com.example.crud.dto.reservation.ReservationData;
 import com.example.crud.dto.reservation.ReservationRegisterParam;
 import com.example.crud.dto.reservation.ReservationUpdateParam;
 import com.example.crud.dto.reservation.ReservationSearchRequest;
@@ -17,6 +18,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 import static com.example.crud.exception.ErrorCode.SEAT_IS_FULL;
 import static com.example.crud.exception.ErrorCode.VALUE_NOT_FOUND;
 
@@ -28,31 +32,35 @@ public class ReservationService {
     private final ShopJpaRepository shopJpaRepository;
     private final RedisRepository redisRepository;
     private final RedisKeyUtil redisKeyUtil;
+    private final Logger log = Logger.getLogger(ReservationService.class.getName());
     public ApiResponse<?> registerReservation(ReservationRegisterParam param) {
 
         shopCheck(param.getShopId());
-        seatCheck(shopJpaRepository.findById(param.getShopId()).get(), param.getDate().toString(), param.getTime());
-        String Key = redisKeyUtil.getReservationKey(param.getShopId(), param.getDate().toString(), param.getTime());
-        distributeLockExecutor.execute("reservation_lock"+Key,10000,10000,()->{
-            Reservation reservation = param.toDomain();
-            redisRepository.addReservation(param.getShopId(), param.getDate().toString(), param.getTime(), param.getMemberNo());
 
+        String Key = redisKeyUtil.getReservationKey(param.getShopId(), param.getDate().toString(), param.getTime());
+        Reservation reservation = param.toDomain();
+        distributeLockExecutor.execute("reservation_lock"+Key,10000,10000,()->{
+            seatCheck(shopJpaRepository.findById(param.getShopId()).get(), param.getDate().toString(), param.getTime());
+
+            redisRepository.addReservation(param.getShopId(), param.getDate().toString(), param.getTime(), param.getMemberNo());
             reservationJpaRepository.save(reservation);
         });
-        //Redis 저장
+
+
         return ApiResponse.of("SUCCESS");
+    }
+    public void syncReservationFromRedis(Integer shopId, String date, String time) {
+    //redis에 값이 있는지 확인 후 해당 값을 db에 저장
+
     }
     public void shopCheck(Integer shopId) {
         shopJpaRepository.findById(shopId)
                 .orElseThrow(() -> new CrudException(VALUE_NOT_FOUND, "Shop not found"));
     }
     public void seatCheck(Shop shop, String date, String time) {
-        // Redis에서 현재 예약 개수 조회
         Integer count = redisRepository.getReservationCount(shop.getId(), date, time);
-        // 현재 예약된 좌석 개수 (없으면 0으로 처리)
         int reservationCount = (count == null) ? 0 : count;
         System.out.println("카운트:"+count);
-        // 좌석 초과 여부 체크
         if (shop.getSeat() <= reservationCount) {
             throw new CrudException(SEAT_IS_FULL, "Seat is full");
         }
